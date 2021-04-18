241cd47951d60ad67ffe5b6c62823e61558c7505
Sloane Zhang <SloanZhang303@gmail.com>
2021-04-17 23:51:00 -0230

    no message

--- src/components/CategorySection.js
+++ src/components/CategorySection.js
@@ -11,106 +11,74 @@ export const Grid = styled.div`
     display: grid;
     padding: 5% 10%;
     grid-template-columns: repeat(3, 1fr);
-    grid-template-rows: 500px;
+    
     grid-gap: 60px;
+    @media screen and (max-width: 768px){
+        grid-template-columns: 1fr;
+        grid-template-rows: 400px 400px 400px;
+    }
 `;
 
 export const Item = styled.div`
     display: flex;
     box-sizing: border-box;
-    justify-content: center;
     padding: .5rem;
-    color: black;
-    background-color: pink;
-
 
     position: relative;
-    z-index: 10;
-    display: flex;
-    flex-direction: column;
-    max-width: 1600px;
-
+    justify-content: center;
     align-items: center;
+    flex-direction: column;
+
+    background-color: pink;
+    z-index: 10;
+    max-width: 1000px;
+
     align-items: flex-end;
     overflow: hidden;
     padding: 1rem;
     width: 100%;
-    text-align: center;
-    color: whitesmoke;
-    background-color: whitesmoke;
+
     box-shadow: 0 1px 1px rgba(0,0,0,0.1), 
         0 2px 2px rgba(0,0,0,0.1), 
         0 4px 4px rgba(0,0,0,0.1), 
         0 8px 8px rgba(0,0,0,0.1),
         0 16px 16px rgba(0,0,0,0.1);
-  
-  @media (min-width: $bp-md) {
-    height: 350px;
-  }
 
-  &:before {
-    content: '';
+
+`;
+
+const CategoryImage = styled.img`
     position: absolute;
     top: 0;
     left: 0;
-    width: 100%;
-    height: 110%;
-    background-size: cover;
-    background-position: 0 0;
-    transition: transform calc(var(--d) * 1.5) var(--e);
-    pointer-events: none;
-  }
+    width: 100vw;
+    height: 100vh;
 
-  &:after {
-    content: '';
+    max-width:100%;
+    max-height:100%;
+    margin: auto;
     display: block;
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 200%;
-    pointer-events: none;
-    background-image: linear-gradient(
-      to bottom,
-      hsla(0, 0%, 0%, 0) 0%,
-      hsla(0, 0%, 0%, 0.009) 11.7%,
-      hsla(0, 0%, 0%, 0.034) 22.1%,
-      hsla(0, 0%, 0%, 0.072) 31.2%,
-      hsla(0, 0%, 0%, 0.123) 39.4%,
-      hsla(0, 0%, 0%, 0.182) 46.6%,
-      hsla(0, 0%, 0%, 0.249) 53.1%,
-      hsla(0, 0%, 0%, 0.320) 58.9%,
-      hsla(0, 0%, 0%, 0.394) 64.3%,
-      hsla(0, 0%, 0%, 0.468) 69.3%,
-      hsla(0, 0%, 0%, 0.540) 74.1%,
-      hsla(0, 0%, 0%, 0.607) 78.8%,
-      hsla(0, 0%, 0%, 0.668) 83.6%,
-      hsla(0, 0%, 0%, 0.721) 88.7%,
-      hsla(0, 0%, 0%, 0.762) 94.1%,
-      hsla(0, 0%, 0%, 0.790) 100%
-    );
-    transform: translateY(-50%);
-    transition: transform calc(var(--d) * 2) var(--e);
-  }
-  
+    z-index: -2;
+`
+const CategoryTitle = styled.div`
+    height: 80%;
     h1{
+        text-align: right;
         font-size: clamp(1rem, 8vw, 2rem);
         font-weight: 400;
         text-transform: uppercase;
         text-shadow: 0px 0px 20px rgba(0, 0, 0, 0.4);
-        text-align: left;
-        margin-bottom: 0.8rem;
-    }
 
+    }
+`
+const CategoryDescription = styled.div`
+
+    height: 20%;
     p {
-        margin-bottom: 1.2rem;
+        
         text-shadow: 0px 0px 20px rgba(0, 0, 0, 0.4);
     }
-
-`;
-
-
-
+`
 
 const CategorySection = ({category}) => {
     return (
@@ -118,10 +86,19 @@ const CategorySection = ({category}) => {
             <Grid>
                 {category.map(
                     (item, index) =>(
-                        <Item>
-                            <h1>{item.title}</h1>
-                            <p>{item.paragraph}</p>
 
+                        <Item>
+                            <CategoryImage src={item.image}>
+                            </CategoryImage>
+                            <CategoryTitle>
+                                <h1>{item.title}</h1>
+
+                            </CategoryTitle>
+                            <CategoryDescription>
+                                <p>{item.paragraph}</p>
+                            </CategoryDescription>
+                            
+                        
                         </Item>
                     )
                 )}
