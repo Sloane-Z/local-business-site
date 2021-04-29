32ddec045b141f9db8893c7bd58087c663f18a19
Sloane-Z <50128500+Sloane-Z@users.noreply.github.com>
2021-04-29 17:02:04 -0230

    no message

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -271,7 +271,7 @@ export class Contact extends React.Component {
 };
 
   componentDidMount() {
-    fetch('backend.stjohnslocalguide.com/v1/emailAPIKey')
+    fetch('http://backend.stjohnslocalguide.com/v1/emailAPIKey')
     .then(res => res.json()).then((data) => {
       this.state({emailAPIKey: data})
     })
